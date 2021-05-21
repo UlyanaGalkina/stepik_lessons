f531f2e2cac3c6afd70a74fdc9e4ba08a1d3df81
Galkina <galkina@report.finance>
2021-05-21 06:03:02 +0300

    added correct data

--- module_2/test_registration.py
+++ module_2/test_registration.py
@@ -1,27 +1,41 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from sys import argv
 import time
 
-link = "http://suninjuly.github.io/simple_form_find_task.html"
+script_name, link = argv
 
 try:
     browser = webdriver.Chrome()
+    # browser.get("http://suninjuly.github.io/registration1.html")
     browser.get(link)
 
-    input1 = browser.find_element_by_name("first_name")
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element(By.XPATH, "//input[@placeholder='Input your first name']")
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_name("last_name")
+    input2 = browser.find_element(By.XPATH, "//input[@placeholder='Input your last name']")
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name("city")
-    input3.send_keys("Smolensk")
-    input4 = browser.find_element_by_id("country")
-    input4.send_keys("Russia")
+    input3 = browser.find_element(By.XPATH, "//input[@placeholder='Input your email']")
+    input3.send_keys("test@test.ru")
+
+    # Отправляем заполненную форму
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
 
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
 finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(30)
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+
     # закрываем браузер после всех манипуляций
     browser.quit()
-
-# не забываем оставить пустую строку в конце файла
